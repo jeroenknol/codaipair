5210357ceea1b8abf4a2a11b7ad637f0b0eab966
jeroenknol <jeroen-knol@live.nl>
2017-02-22 21:39:04 +0100

    Add currentUser to state

--- src/reducers/current-user.js
+++ src/reducers/current-user.js
@@ -1,13 +1,14 @@
 import { USER_SIGNED_IN } from '../actions/user/sign-in'
 import { USER_SIGNED_OUT } from '../actions/user/sign-out'
 
-const CURRENT_USER_KEY = 'recipes.currentUser'
+const CURRENT_USER_KEY = 'codaipair.currentUser'
 
 const initialState = JSON.parse(localStorage.getItem(CURRENT_USER_KEY))
 
 export default (state = initialState, { type, payload }) => {
   switch(type) {
     case USER_SIGNED_IN :
+      console.log('Hello there', initialState)
       const currentUser = Object.assign({}, payload)
       localStorage.setItem(CURRENT_USER_KEY, JSON.stringify(currentUser))
       return currentUser
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 // src/reducers/index.js
 import loadError from './load-error'
 import loading from './loading'
+import currentUser from './current-user'
 
 module.exports = {
   loadError,
   loading,
+  currentUser,
 }
